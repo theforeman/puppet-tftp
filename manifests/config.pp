0b6d430d40e2c4db4cd79f14a5b24786814cc9e6
Alexander Fisher <alex@linfratech.co.uk>
2017-08-16 15:30:15 +0100

    Fix `invalid byte sequence in UTF-8` error
    Previously the catalog compiled, but did not apply.  Applying the file
    resource would fail with `invalid byte sequence in UTF-8` when trying to
    display the diff.

--- manifests/config.pp
+++ manifests/config.pp
@@ -29,9 +29,10 @@ class tftp::config {
       }
 
       file {'/etc/tftpd.map':
-        source => "puppet:///modules/${module_name}/tftpd.map",
-        mode   => '0644',
-        notify => Class['xinetd'],
+        source    => "puppet:///modules/${module_name}/tftpd.map",
+        mode      => '0644',
+        show_diff => false, # Puppet explodes with 'Error: invalid byte sequence in UTF-8' when trying to display the diff
+        notify    => Class['xinetd'],
       }
 
       file { $::tftp::root:
