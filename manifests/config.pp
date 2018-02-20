d1cd16d83f9864b6b84dbb2a2e0409756793bc7c
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2018-02-20 10:33:08 +0100

    Refactor to use daemon as a boolean
    Since $::tftp::daemon is a boolean, we can simplify the case statement
    to a simple if/else.

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,44 +1,41 @@
 # Configure TFTP
 class tftp::config {
 
-  case $::tftp::daemon {
-    default: {
-      if $tftp::manage_root_dir {
-        ensure_resource('file', $::tftp::root, {'ensure' => 'directory'})
-      }
+  if $::tftp::daemon {
+    if $tftp::manage_root_dir {
+      ensure_resource('file', $::tftp::root, {'ensure' => 'directory'})
+    }
 
-      if $::osfamily =~ /^(FreeBSD|DragonFly)$/ {
-        augeas { 'set root directory':
-          context => '/files/etc/rc.conf',
-          changes => "set tftpd_flags '\"-s ${::tftp::root}\"'",
-        }
+    if $::osfamily =~ /^(FreeBSD|DragonFly)$/ {
+      augeas { 'set root directory':
+        context => '/files/etc/rc.conf',
+        changes => "set tftpd_flags '\"-s ${::tftp::root}\"'",
       }
     }
-    false: {
-      include ::xinetd
+  } else {
+    include ::xinetd
 
-      xinetd::service { 'tftp':
-        port        => '69',
-        server      => '/usr/sbin/in.tftpd',
-        server_args => "-v -s ${::tftp::root} -m /etc/tftpd.map",
-        socket_type => 'dgram',
-        protocol    => 'udp',
-        cps         => '100 2',
-        flags       => 'IPv4',
-        per_source  => '11',
-      }
+    xinetd::service { 'tftp':
+      port        => '69',
+      server      => '/usr/sbin/in.tftpd',
+      server_args => "-v -s ${::tftp::root} -m /etc/tftpd.map",
+      socket_type => 'dgram',
+      protocol    => 'udp',
+      cps         => '100 2',
+      flags       => 'IPv4',
+      per_source  => '11',
+    }
 
-      file {'/etc/tftpd.map':
-        source    => "puppet:///modules/${module_name}/tftpd.map",
-        mode      => '0644',
-        show_diff => false, # Puppet explodes with 'Error: invalid byte sequence in UTF-8' when trying to display the diff
-        notify    => Class['xinetd'],
-      }
+    file {'/etc/tftpd.map':
+      source    => "puppet:///modules/${module_name}/tftpd.map",
+      mode      => '0644',
+      show_diff => false, # Puppet explodes with 'Error: invalid byte sequence in UTF-8' when trying to display the diff
+      notify    => Class['xinetd'],
+    }
 
-      file { $::tftp::root:
-        ensure => directory,
-        notify => Class['xinetd'],
-      }
+    file { $::tftp::root:
+      ensure => directory,
+      notify => Class['xinetd'],
     }
   }
 }
