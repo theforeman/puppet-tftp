ab05ee38c661bf28847c9e1471d12ffb376a0906
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2018-09-06 00:53:48 +0200

    Simplify service class
    Since we know $daemon is a boolean, we can use a simple if statement
    instead of a case. We also know that in init.pp we already notify so
    there's no need to subscribe again.

--- manifests/service.pp
+++ manifests/service.pp
@@ -2,16 +2,12 @@
 class tftp::service {
 
   # No service needed if not daemonized
-  case $tftp::daemon {
-    false: { }
-    default: {
-      service { $tftp::service:
-        ensure    => running,
-        enable    => true,
-        alias     => 'tftpd',
-        provider  => $tftp::service_provider,
-        subscribe => Class['tftp::config'],
-      }
+  if $tftp::daemon {
+    service { $tftp::service:
+      ensure   => running,
+      enable   => true,
+      alias    => 'tftpd',
+      provider => $tftp::service_provider,
     }
   }
 }
