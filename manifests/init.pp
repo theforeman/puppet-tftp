2efc25128d1a9004c4b6583bd1bb539a5b162103
Benjamin Rechsteiner <benjamin.rechsteiner@abacus.ch>
2019-01-30 12:22:14 +0100

    Add daemon config file on Debian

--- manifests/init.pp
+++ manifests/init.pp
@@ -26,6 +26,10 @@
 # @param map_source The source URL of the mapping file
 # @param service Name of the TFTP service, when daemon is true
 # @param service_provider Override TFTP service provider, when daemon is true
+# @param username Configures the daemon user
+# @param port Configures the Listen Port
+# @param address Configures the Listen Address, if empty it will listen on IPv4 and IPv6 (only on tftpd-hpa)
+# @param options Configures daemon options
 class tftp (
   Stdlib::Absolutepath $root,
   String $package,
@@ -35,6 +39,10 @@ class tftp (
   String $map_source,
   Optional[String] $service = undef,
   Optional[String] $service_provider = undef,
+  String $username = 'root',
+  Stdlib::Port $port = 69,
+  Optional[String] $address = undef,
+  Optional[String] $options = undef,
 ) {
   contain tftp::install
   contain tftp::config
